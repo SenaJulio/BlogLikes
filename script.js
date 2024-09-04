b1e345905706d671615bac9bd1375d942282e338
JULIO SENA <gm.sena@hotmail.com>
2024-09-04 10:34:18 -0300

    blog likes

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ async  function getPosts(){
     button.innerHTML = 'Like';
 
     let small = document.createElement('small');
-    small.innerHTML = '99 liks'
+    let likeCount = getLikesFromId(post.id)
+    small.innerHTML = likeCount + ' liks'
 
     let article = document.createElement('article');
     article.appendChild(h2)
@@ -35,6 +36,17 @@ async  function getPosts(){
     alert("Não conseguimos carregar os posts");
     
  }
+ }
+
+ function getLikesFromId(id) {
+   let likesString = localStorage.getItem('likes');
+   if(!likesString) return 0;
+
+   let likes = JSON.parse(likesString);
+   let postLike = likes.find(item => item.id === id);
+   if(!postLike) return 0;
+
+   return postLike.count;
  }
  
   getPosts();
